5ee8a016f6c8c883eecf6f2a812ef96e3950ee6a
dhanush-business <dhanush.business01@gmail.com>
2025-10-28 10:53:10 +0530

    Update profile.js

--- web/profile.js
+++ web/profile.js
@@ -1,5 +1,5 @@
 // --- Backend URL ---
-const BACKEND_URL = 'http://127.0.0.1:5000'; // Relative path
+const BACKEND_URL = 'http://0.0.0.0/0'; // Relative path
 
 // --- Default Avatar Path (Served by Frontend/Vercel) ---
 const DEFAULT_AVATAR_STATIC_PATH = "/avatars/default_avatar.png";
@@ -164,3 +164,4 @@ async function saveProfileChanges() {
     }
 
 }
+
